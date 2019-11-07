3905ffed45c451b2caccb50809ad80657fe66613
Kellie Henderson <kellie.henderson@my.tccd.edu>
2019-11-07 20:58:07 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  int string = " " ;
+    if ( id == 1 )
+{
+    cout << "Martha" << endl;
+}
+else
+{
+
+    if ( id == 2 || id == 3 )
+    {
+        cout << "David" <<endl;
+    }
+    else
+    {
+        if ( id == 4 )
+        {
+            cout << "Green Tea" << endl;
+        }
+        else
+        {
+            cout << "Commit to Complete" << endl;
+        }
+    }
+}
+}
